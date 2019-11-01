d6b90646c93f682c0a0ccc569c2963f4c905ec53
horea.bucerzan <horea.bucerzan@gmail.com>
2019-11-01 18:07:29 +0200

    move rep to new comp

--- src/main/java/libgdx/game/LettersGame.java
+++ src/main/java/libgdx/game/LettersGame.java
@@ -2,11 +2,13 @@ package libgdx.game;
 
 
 import libgdx.constants.GameIdEnum;
+import libgdx.dbapi.UniqueDbOperationContainer;
 import libgdx.game.external.AppInfoService;
 import libgdx.game.external.BillingService;
 import libgdx.game.external.FacebookService;
 import libgdx.game.external.LoginService;
 import libgdx.implementations.letter.LettersGameDependencyManager;
+import libgdx.login.GuestUserLoginService;
 import libgdx.screens.AbstractScreen;
 import libgdx.services.ScreenManager;
 
@@ -26,6 +28,17 @@ public class LettersGame extends Game<AppInfoService,
 
     public LettersGameDependencyManager getDependencyManager() {
         return getSubGameDependencyManager();
+    }
+
+    @Override
+    public void create() {
+        super.create();
+        loginService = createLoginService();
+        UniqueDbOperationContainer.getInstance().clear();
+    }
+
+    private LoginService createLoginService() {
+        return new GuestUserLoginService();
     }
 
     public static LettersGame getInstance() {
